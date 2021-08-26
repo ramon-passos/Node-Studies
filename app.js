20e42f312e04b0c38ca49cf838488698d8b54d32
ramon-passos <ramonpassos2011@gmail.com>
2021-08-25 23:21:49 -0300

    setting up app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const http = require('http');
+
+const { hostname } = require('os');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    req.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plan');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
 
